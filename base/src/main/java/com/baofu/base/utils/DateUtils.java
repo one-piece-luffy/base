e96a162823db1d7cbd1a63a09cec6da7656c03fc
lhyi <lihaiyi@yousuo.net>
2020-09-24 14:37:27 +0800

    no message

--- base/src/main/java/com/baofu/base/utils/DateUtils.java
+++ base/src/main/java/com/baofu/base/utils/DateUtils.java
@@ -1,13 +1,16 @@
 package com.baofu.base.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateUtils {
 
     /**
      * 将时间戳转换为时间
-     * @param time 时间戳
+     * @param time 时间戳 单位毫秒
      * @param formatString 格式化样式 "yyyy-MM-dd HH:mm"
      * @return
      */
@@ -15,5 +18,75 @@ public class DateUtils {
         SimpleDateFormat sdf=new SimpleDateFormat(formatString);
         String sd = sdf.format(new Date(Long.parseLong(String.valueOf(time))));
         return sd;
+    }
+
+    /*
+     * 将时间转换为时间戳
+     */
+    public static long dateToStamp(String s) throws ParseException {
+        String res;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = simpleDateFormat.parse(s);
+        long ts = date.getTime();
+        return ts;
+    }
+
+
+    /**
+
+     * 计算两个日期之间相差的天数
+
+     * @param smdate 较小的时间
+
+     * @param bdate 较大的时间
+
+     * @return 相差天数
+
+     * @throws ParseException
+
+     */
+
+    public static int daysBetween(Date smdate,Date bdate) throws ParseException {
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTime(smdate);
+
+        long time1 = cal.getTimeInMillis();
+
+        cal.setTime(bdate);
+
+        long time2 = cal.getTimeInMillis();
+
+        long between_days=(time2-time1)/(1000*3600*24);
+
+        return Integer.parseInt(String.valueOf(between_days));
+
+    }
+
+    public static String formatTime( String time, String formatString) {
+        if (time == null || "".equals(time)) {
+            return "";
+        }
+        String str = "";
+        SimpleDateFormat format = new SimpleDateFormat(formatString);
+        format.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+        try {
+
+            Calendar cal = Calendar.getInstance();
+            int localDay = cal.get(Calendar.DAY_OF_YEAR);
+
+            Date d = format.parse(time);
+            format = new SimpleDateFormat(formatString);
+
+            // format.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+            format.setTimeZone(Calendar.getInstance().getTimeZone());
+//            format.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+            str = format.format(d);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return str;
     }
 }
--- base/src/main/java/com/baofu/base/utils/ShareUtils.java
+++ base/src/main/java/com/baofu/base/utils/ShareUtils.java
@@ -13,7 +13,7 @@ import java.io.File;
 public class ShareUtils {
 
 
-    public static void shareSystemFile(Context context, String filePath) {
+    public static void shareSystemFile(Context context, String appId,String filePath) {
         File file = new File(filePath);
         if (!file.exists()) {
             AppUtils.showToast("file dont exists");
@@ -30,7 +30,7 @@ public class ShareUtils {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // 添加这一句表示对目标应用临时授权该Uri所代表的文件
                 audioUri = FileProvider.getUriForFile(context,
-                        "com.ysit.new_monitor" + ".fileProvider", file);// 通过FileProvider创建一个content类型的Uri
+                        appId + ".fileProvider", file);// 通过FileProvider创建一个content类型的Uri
             } else {
                 audioUri = Uri.fromFile(file);
             }
@@ -42,5 +42,29 @@ public class ShareUtils {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public static void shareQqFile(Context context,String appid,String filePath){
+        File file = new File(filePath);
+        if (!file.exists()) {
+            AppUtils.showToast("file dont exists");
+            return;
+        }
+
+        ComponentName comp = new ComponentName("com.tencent.mobileqq",
+                "com.tencent.mobileqq.activity.JumpActivity");
+        Intent shareIntent = new Intent();
+        Uri audioUri = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // 添加这一句表示对目标应用临时授权该Uri所代表的文件
+            audioUri = FileProvider.getUriForFile(context,
+                    appid + ".fileProvider", file);// 通过FileProvider创建一个content类型的Uri
+        } else {
+            audioUri = Uri.fromFile(file);
+        }
+        shareIntent.setComponent(comp);
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, audioUri);
+        shareIntent.setType("*/*");
+        context.startActivity(Intent.createChooser(shareIntent, "file"));
     }
 }
